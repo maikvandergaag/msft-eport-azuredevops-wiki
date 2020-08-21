9479fe4f4e7da2d00c8df0ccc2c3e6d37de36c61
MSFTPlayground <maik@familie-vandergaag.nl>
2020-08-21 22:03:55 +0200

    moved function

--- scripts/singlepage-wiki.ps1
+++ scripts/singlepage-wiki.ps1
@@ -3,53 +3,55 @@ Param (
     [Parameter(Mandatory = $false, Position = 0)][string]$FolderName = "wiki"
 )
 
-function ProcessWikiFolder { 
-    Param (
-        [Parameter(Mandatory = $true, Position = 0)][string]$Folder,
-        [Parameter(Mandatory = $true, Position = 0)][string]$OutputFile
-    )
-    Write-Output "## start processing folder: $($Folder)" 
-    $orderItem = Get-ChildItem $Folder -File -Force | Where-Object { $_.Extension -eq ".order" }
-          
-    if ($orderItem) {
-        $orderItemLines = Get-Content $orderItem
-      
-        foreach ($line in $orderItemLines) {
-            Write-Output "### Processing file: $($line)" 
-      
-            $filename = "$($line).md" 
-            $file = Get-ChildItem $Folder -File $filename
-                  
-            if ($file) {
-                $content = Get-Content $file
-      
-                "# $($line)" | Out-File -FilePath $OutputFile -Append
-                "" | Out-File -FilePath $OutputFile -Append
-      
-                $content | Out-File -FilePath $outputFile -Append
-      
-                "" | Out-File -FilePath $OutputFile -Append
-                "" | Out-File -FilePath $OutputFile -Append
-                "" | Out-File -FilePath $OutputFile -Append
-            }
-      
-                  
-            $possiblePath = "$($folder)$($line)\"
-      
-            if (Test-Path $possiblePath) {
-                ProcessWikiFolder -Folder $possiblePath -OutputFile $OutputFile
-            }
-        }
-    }
-    else {
-        Write-Output "order file does not exits"
-    }
-}
+
 
 BEGIN {
     Write-Host "Starting to create a single page wiki"
 }
-PROCESS {           
+PROCESS { 
+    function ProcessWikiFolder { 
+        Param (
+            [Parameter(Mandatory = $true, Position = 0)][string]$Folder,
+            [Parameter(Mandatory = $true, Position = 0)][string]$OutputFile
+        )
+        Write-Output "## start processing folder: $($Folder)" 
+        $orderItem = Get-ChildItem $Folder -File -Force | Where-Object { $_.Extension -eq ".order" }
+              
+        if ($orderItem) {
+            $orderItemLines = Get-Content $orderItem
+          
+            foreach ($line in $orderItemLines) {
+                Write-Output "### Processing file: $($line)" 
+          
+                $filename = "$($line).md" 
+                $file = Get-ChildItem $Folder -File $filename
+                      
+                if ($file) {
+                    $content = Get-Content $file
+          
+                    "# $($line)" | Out-File -FilePath $OutputFile -Append
+                    "" | Out-File -FilePath $OutputFile -Append
+          
+                    $content | Out-File -FilePath $outputFile -Append
+          
+                    "" | Out-File -FilePath $OutputFile -Append
+                    "" | Out-File -FilePath $OutputFile -Append
+                    "" | Out-File -FilePath $OutputFile -Append
+                }
+          
+                      
+                $possiblePath = "$($folder)$($line)\"
+          
+                if (Test-Path $possiblePath) {
+                    ProcessWikiFolder -Folder $possiblePath -OutputFile $OutputFile
+                }
+            }
+        }
+        else {
+            Write-Output "order file does not exits"
+        }
+    }
+              
     $path = "$(System.DefaultWorkingDirectory)/$($FolderName)"
     if ($path -notmatch '\\$') { $path += '/' }
     
